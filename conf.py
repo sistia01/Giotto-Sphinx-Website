990d9bf40f10d2b30cb9eab1d1275dc20164f040
Adriana Sistig <59894711+sistia01@users.noreply.github.com>
2021-08-27 15:06:48 -0400

    Add files via upload

--- conf.py
+++ conf.py
@@ -34,10 +34,7 @@ import sphinx_rtd_theme
 
 extensions = ['sphinx.ext.autosectionlabel', 
 'sphinx.ext.autodoc',
-#'sphinx_panels',
-#'sphinx_togglebutton',
 'sphinx_rtd_theme',
-#'sphinx_tabs',
 'sphinx-design'
 ]
 
